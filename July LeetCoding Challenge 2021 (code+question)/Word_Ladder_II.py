e7121d9532e669298ec433e6fd51883613d0a358
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-07-24 15:38:36 +0530

    Create Word_Ladder_II.py

---
+++ July LeetCoding Challenge 2021 (code+question)/Word_Ladder_II.py
@@ -0,0 +1,62 @@
+'''
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words
+beginWord -> s1 -> s2 -> ... -> sk such that:
+Every adjacent pair of words differs by a single letter. Every si for 1 <= i <= k is in wordList. Note that beginWord
+does not need to be in wordList. sk == endWord Given two words, beginWord and endWord, and a dictionary wordList,
+return all the shortest transformation sequences from beginWord to endWord, or an empty list if no such sequence
+exists. Each sequence should be returned as a list of the words [beginWord, s1, s2, ..., sk].
+
+Example 1:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: [["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+Explanation: There are 2 shortest transformation sequences:
+"hit" -> "hot" -> "dot" -> "dog" -> "cog"
+"hit" -> "hot" -> "lot" -> "log" -> "cog"
+
+Example 2:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: []
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+Constraints:
+1 <= beginWord.length <= 5
+endWord.length == beginWord.length
+1 <= wordList.length <= 1000
+wordList[i].length == beginWord.length
+beginWord, endWord, and wordList[i] consist of lowercase English letters.
+beginWord != endWord
+All the words in wordList are unique.
+'''
+
+
+class Solution:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        wordList += [beginWord]
+        n, k = len(wordList), len(wordList[0])
+        patterns = defaultdict(set)
+        for word in wordList:
+            for ind in range(0, k):
+                tmp = word[0:ind] + "*" + word[ind + 1:]
+                patterns[tmp].add(word)
+
+        G = defaultdict(set)
+        for elem in patterns.values():
+            for x, y in permutations(elem, 2):
+                G[x].add(y)
+
+        deps = {w: -1 for w in wordList}
+        deps[beginWord] = 0
+        paths = defaultdict(list)
+        paths[beginWord] = [[beginWord]]
+        queue = deque([beginWord])
+
+        while queue:
+            w = queue.popleft()
+            if w == endWord: return paths[w]
+            for neib in G[w]:
+                if deps[neib] == -1 or deps[neib] == deps[w] + 1:
+                    if deps[neib] == -1:
+                        queue.append(neib)
+                        deps[neib] = deps[w] + 1
+                    for elem in paths[w]:
+                        paths[neib].append(elem + [neib])
