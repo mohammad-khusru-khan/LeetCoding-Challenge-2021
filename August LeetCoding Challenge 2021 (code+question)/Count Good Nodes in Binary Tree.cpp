03fe216f1aafd30ed28579858f90ffcafc0c7393
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-08-17 12:45:12 +0530

    Create Count Good Nodes in Binary Tree.cpp

---
+++ August LeetCoding Challenge 2021 (code+question)/Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,64 @@
+/*
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+ 
+Example 1:
+
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+ 
+
+Constraints:
+
+The number of nodes in the binary tree is in the range [1, 10^5].
+Each node's value is between [-10^4, 10^4].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    int CountGoodNodes(TreeNode *root, int M)
+    {
+        if (root == NULL)
+            return 0;
+        int count = 0;
+        if (M <= root->val)
+            count++;
+        M = max(M, root->val);
+        return CountGoodNodes(root->left, M) + CountGoodNodes(root->right, M) + count;
+    }
+    int goodNodes(TreeNode *root)
+    {
+        int M = -10001;
+        return CountGoodNodes(root, M);
+    }
+};
