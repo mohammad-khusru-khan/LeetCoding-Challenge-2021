2d54b52c99da7589f027773bcd72635af16b0e7a
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-08-20 13:25:30 +0530

    Create Valid Sudoku.py

---
+++ August LeetCoding Challenge 2021 (code+question)/Valid Sudoku.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def notInRow(arr, row):
+            st = set()
+            for i in range(0, 9):
+                if arr[row][i] in st:
+                    return False
+                if arr[row][i] != '.':
+                    st.add(arr[row][i])
+
+            return True
+
+        def notInCol(arr, col):
+            st = set()
+            for i in range(0, 9):
+                if arr[i][col] in st:
+                    return False
+                if arr[i][col] != '.':
+                    st.add(arr[i][col])
+
+            return True
+
+        def notInBox(arr, startRow, startCol):
+            st = set()
+            for row in range(0, 3):
+                for col in range(0, 3):
+                    curr = arr[row + startRow][col + startCol]
+                    if curr in st:
+                        return False
+                    if curr != '.':
+                        st.add(curr)
+
+            return True
+
+        def isValid(arr, row, col):
+            return notInRow(arr, row) and notInCol(arr, col) and notInBox(arr, row - row % 3, col - col % 3)
+
+        def isValidConfig(arr, n):
+            for i in range(0, n):
+                for j in range(0, n):
+                    if not isValid(arr, i, j):
+                        return False
+
+            return True
+
+        if isValidConfig(board, 9):
+            return True
+        else:
+            return False
