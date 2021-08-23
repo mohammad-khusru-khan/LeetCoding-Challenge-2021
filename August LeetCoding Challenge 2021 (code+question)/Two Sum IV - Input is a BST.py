268f4f68de82ace431bc185f4dd0518dc1c8b094
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-08-23 13:34:42 +0530

    Create Two Sum IV - Input is a BST.py

---
+++ August LeetCoding Challenge 2021 (code+question)/Two Sum IV - Input is a BST.py
@@ -0,0 +1,60 @@
+'''
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+Example 3:
+
+Input: root = [2,1,3], k = 4
+Output: true
+Example 4:
+
+Input: root = [2,1,3], k = 1
+Output: false
+Example 5:
+
+Input: root = [2,1,3], k = 3
+Output: true
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        visited = set()
+
+        def GetPair(root, visited, target):
+            if root == None:
+                return False
+            if GetPair(root.left, visited, target):
+                return True
+            if target - root.val in visited:
+                return True
+            else:
+                visited.add(root.val)
+            return GetPair(root.right, visited, target)
+
+        return GetPair(root, visited, k)
