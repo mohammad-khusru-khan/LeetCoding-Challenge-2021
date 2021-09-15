198ba4d9d0d12ff96253ea0654fa0e6b2d8ea89f
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-09-15 12:43:55 +0530

    Longest Turbulent Subarray

---
+++ September LeetCoding Challenge 2021 (code+question)/Longest_Turbulent_Subarray.py
@@ -0,0 +1,57 @@
+'''
+Given an integer array arr, return the length of a maximum size turbulent subarray of arr.
+
+A subarray is turbulent if the comparison sign flips between each adjacent pair of elements in the subarray.
+
+More formally, a subarray [arr[i], arr[i + 1], ..., arr[j]] of arr is said to be turbulent if and only if:
+
+For i <= k < j:
+arr[k] > arr[k + 1] when k is odd, and
+arr[k] < arr[k + 1] when k is even.
+Or, for i <= k < j:
+arr[k] > arr[k + 1] when k is even, and
+arr[k] < arr[k + 1] when k is odd.
+
+
+Example 1:
+
+Input: arr = [9,4,2,10,7,8,8,1,9]
+Output: 5
+Explanation: arr[1] > arr[2] < arr[3] > arr[4] < arr[5]
+Example 2:
+
+Input: arr = [4,8,12,16]
+Output: 2
+Example 3:
+
+Input: arr = [100]
+Output: 1
+
+
+Constraints:
+
+1 <= arr.length <= 4 * 104
+0 <= arr[i] <= 109
+'''
+
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        flip, count, curr = False, 1, 1
+        for i in range(len(arr) - 1):
+            if arr[i] == arr[i + 1]:
+                curr = 1
+                continue
+            if arr[i] > arr[i + 1]:
+                if flip:
+                    curr = 2
+                else:
+                    curr += 1
+                flip = True
+            if arr[i] < arr[i + 1]:
+                if flip:
+                    curr += 1
+                else:
+                    curr = 2
+                flip = False
+            count = max(count, curr)
+        return count
