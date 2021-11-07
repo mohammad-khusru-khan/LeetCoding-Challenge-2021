0c3288f82eb037038649eef7e695a7bda58e5f5e
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-11-07 09:46:14 +0530

    Multiply Strings

---
+++ November LeetCoding Challenge 2021 (code+question)/Multiply_Strings.py
@@ -0,0 +1,38 @@
+'''
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ 
+
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+ 
+
+Constraints:
+
+1 <= num1.length, num2.length <= 200
+num1 and num2 consist of digits only.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+'''
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        m, n = len(num1), len(num2)
+        d = Counter()
+        for i in range(m):
+            for j in range(n):
+                d[i+j] += int(num1[m-1-i])*int(num2[n-1-j])
+                
+        carry, ans = 0, ""
+        for i in range(m+n-1):
+            carry, digit = divmod(carry + d[i], 10)
+            ans += str(digit)
+            
+        ans = (str(carry) + ans[::-1]).lstrip("0")
+        return ans if ans != "" else "0"
