283c43cee1deef17c996e43933fbfd324c9eb02c
mohammad-khusru-khan <67111803+mohammad-khusru-khan@users.noreply.github.com>
2021-12-17 10:25:27 +0530

    Maximal Square

---
+++ December LeetCoding Challenge 2021 (code+question)/Maximal_Square.py
@@ -0,0 +1,37 @@
+'''
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+'''
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        dp, ans = [[0] * len(matrix[0]) for row in matrix], 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                    ans = max(ans, dp[i][j] ** 2)
+        return ans
